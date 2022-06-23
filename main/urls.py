56a28b71c919569e5ec60e3884703ce03007d0c2
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2022-06-23 14:08:26 +0530

    init

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("elaborate/<str:title>",views.elab,name="index"),
-    path("",views.index,name="index")
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,35 +1,41 @@
 from django.shortcuts import render,HttpResponse
 import requests
+from datetime import date 
+import json
 __Data = None
 def elab(request,title):
     global __Data
-    # json_obj = requests.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=6eeec874600546b4be9d3b37d6697ab8")
-    # data = json_obj.json().get("articles")
     for i in __Data:
         if  title in i.get("title") :
             temp = []
             temp.append(i.get("author"))
             temp.append(i.get("title"))
-            temp.append(i.get("urlToImage"))
-            temp.append(i.get("publishedAt"))
-            temp.append(i.get("description"))
-            temp.append(i.get("content"))
+            temp.append(i.get("media"))
+            temp.append(i.get("published_date"))
+            temp.append(i.get("summary"))
+            temp.append('')
             return render(request,"single.html",{"datas":[temp]})
     return HttpResponse("Incorrect Dynamic Link Found")
 def index(request):
     global __Data
-    json_obj = requests.get("https://newsapi.org/v2/everything?q=Technology&apiKey=6eeec874600546b4be9d3b37d6697ab8")
-    data = json_obj.json().get("articles")
-    __Data = data;
+    cur = date.today()
+    params = {
+            'q' : 'technology','lang':'en',
+            'countries':'IN'
+        }
+    headers = {'x-api-key' : 'AzJfTMmZms6634PquqxcUrTF7Z1h0qUKCIlb3d2qn7s'}
+    json_obj = requests.request("GET","https://api.newscatcherapi.com/v2/search",headers = headers,params = params)
+    __Data = json.loads(json_obj.text).get('articles');
     data_list = []
-    for i in data:
-         temp = []
-         temp.append(i.get("author"))
-         temp.append(i.get("title"))
-         temp.append(i.get("urlToImage"))
-         temp.append(i.get("publishedAt"))=
-         data_list.append(temp)
-    return render(request,'index.html',{"obj":data,"content":data_list})
-'''
-[ author, Title Image Date]
-'''    
+    for i in __Data:
+        for j in ("author","title","media","published_date"):
+            if i.get(j) == '' or i.get(j) == None:
+                break
+        else:
+            temp = []
+            temp.append(i.get("author"))
+            temp.append(i.get("title"))
+            temp.append(i.get("media"))
+            temp.append(i.get("published_date"))
+            data_list.append(temp)
+    return render(request,'index.html',{"obj":__Data,"content":data_list})
