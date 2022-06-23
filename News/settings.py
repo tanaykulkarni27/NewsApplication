e3eec33e556529b35f91cc95213b8b5ba872b970
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2022-06-23 10:26:38 +0530

    init

--- News/settings.py
+++ News/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'lfr859)l(i)+&&@m(#6v^(+7g^#qd3g2$)$#uun5$)izsxt084'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','technewapp.herokuapp.com']
 
