3a38747fb572cd8460bc50bcc1fef66c8773886c
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2022-06-23 12:05:41 +0530

    init

--- News/urls.py
+++ News/urls.py
@@ -17,12 +17,7 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf import settings
 
-from django.views.static import serve
-# from django.conf.urls import url
-
 urlpatterns = [
     path('',include("main.urls")),
     path('admin/', admin.site.urls),
-    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
